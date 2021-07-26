0af909dc72039f8b60101b4d6d22c520ec226c7a
RoyRamirez <royramlo13@gmail.com>
2021-07-26 16:29:46 -0500

    se agregó babel al proyecto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Test");
+const objetoPrueba = {
+  prop1: 1,
+  prop2: 2,
+}
+const objetoPrueba2 = {
+  ...objetoPrueba,
+  prop3: 3,
+  prop4: 4,
+}
+console.log(objetoPrueba);
+console.log(objetoPrueba2);
